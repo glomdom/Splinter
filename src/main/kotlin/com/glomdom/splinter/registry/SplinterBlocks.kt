b23f1b24c4c303770a03619e233a7a02610c9644
glomdom <103685817+glomdom@users.noreply.github.com>
2026-09-25 21:26:38 +0300

    feat: redstone output

--- src/main/kotlin/com/glomdom/splinter/registry/SplinterBlocks.kt
+++ src/main/kotlin/com/glomdom/splinter/registry/SplinterBlocks.kt
@@ -3,12 +3,16 @@ package com.glomdom.splinter.registry
 import com.glomdom.splinter.content.machine.Probe
 import com.glomdom.splinter.content.machine.Reader
 import com.glomdom.splinter.content.machine.Receiver
+import com.glomdom.splinter.content.machine.RedstoneOut
 import com.glomdom.splinter.content.machine.data.DataWire
 import org.bukkit.Material
 
 object SplinterBlocks : SplinterRegistry() {
     val READER by block<Reader>(Material.PINK_STAINED_GLASS)
     val RECEIVER by block<Receiver>(Material.GRAY_STAINED_GLASS)
-    val DATA_WIRE by block<DataWire>(Material.STRUCTURE_VOID)
     val PROBE by block<Probe>(Material.RED_STAINED_GLASS)
+
+    val REDSTONE_OUT by block<RedstoneOut>(Material.TARGET)
+
+    val DATA_WIRE by block<DataWire>(Material.STRUCTURE_VOID)
 }
